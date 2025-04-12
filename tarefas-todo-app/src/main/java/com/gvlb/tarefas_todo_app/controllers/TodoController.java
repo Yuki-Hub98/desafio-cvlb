2f8eee56ccb6f17061e6376f561d0a5584fc8514
Yuki-Hub98 <wellyson.yago1@gmail.com>
2025-04-12 09:59:52 -0400

    feat (todo service) - Serviço com rotas para, pesquisar todos os todos, criar, editar e excluir, todos retornam e recebem um DTO padrão

--- tarefas-todo-app/src/main/java/com/gvlb/tarefas_todo_app/controllers/TodoController.java
+++ tarefas-todo-app/src/main/java/com/gvlb/tarefas_todo_app/controllers/TodoController.java
@@ -1,11 +1,37 @@
 package com.gvlb.tarefas_todo_app.controllers;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.gvlb.tarefas_todo_app.DTO.TodoRequestTDO;
+import com.gvlb.tarefas_todo_app.DTO.TodoResponseAllTDO;
+import com.gvlb.tarefas_todo_app.DTO.TodoResponseTDO;
+import com.gvlb.tarefas_todo_app.services.TodoServiceInterface;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/todos")
 public class TodoController {
+
+    @Autowired
+    TodoServiceInterface todoServiceInterface;
+
     @GetMapping
-    public String HelloWorld(){
-        return "helooooow";
+    public ResponseEntity<TodoResponseAllTDO> getAllTodos(){
+        return ResponseEntity.ok(new TodoResponseAllTDO(todoServiceInterface.getAllTodos()));
+    }
+
+    @PostMapping("/create")
+    public  ResponseEntity<TodoResponseTDO> createTodo(@Valid @RequestBody  TodoRequestTDO requestTDO){
+        return ResponseEntity.ok(new TodoResponseTDO(todoServiceInterface.createTodo(requestTDO)));
+    }
+
+    @PutMapping("/edit")
+    public ResponseEntity<TodoResponseTDO> editTodo(@Valid @RequestBody  TodoRequestTDO requestTDO){
+        return ResponseEntity.ok(new TodoResponseTDO(todoServiceInterface.editTodo(requestTDO)));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteTodo(@Valid @PathVariable String id ){
+        return ResponseEntity.ok(todoServiceInterface.deleteTodo(id));
     }
 }
--- tarefas-todo-app/src/main/java/com/gvlb/tarefas_todo_app/services/TodoService.java
+++ tarefas-todo-app/src/main/java/com/gvlb/tarefas_todo_app/services/TodoService.java
@@ -1,4 +1,65 @@
 package com.gvlb.tarefas_todo_app.services;
 
-public class TodoService {
+import com.gvlb.tarefas_todo_app.DTO.TodoRequestTDO;
+import com.gvlb.tarefas_todo_app.configurations.security.SecurityFilter;
+import com.gvlb.tarefas_todo_app.domains.todo.Todo;
+import com.gvlb.tarefas_todo_app.domains.user.User;
+import com.gvlb.tarefas_todo_app.repositories.TodoRepository;
+import com.gvlb.tarefas_todo_app.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService implements TodoServiceInterface{
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @Autowired
+    SecurityFilter securityFilter;
+
+    @Override
+    public List<Todo> getAllTodos(){
+        return todoRepository.findAll();
+    }
+
+    @Override
+    public Todo createTodo(TodoRequestTDO todoRequestTDO){
+        Todo todo = new Todo();
+        return todoRepository.save(transformTodo(todoRequestTDO, todo));
+    }
+
+    @Override
+    public Todo editTodo(TodoRequestTDO todoRequestTDO){
+        Todo todo = todoRepository.findById(Long.parseLong(todoRequestTDO.id()))
+                .orElseThrow(() -> new RuntimeException("Todo não encontrado com ID: " + todoRequestTDO.id()));
+        return todoRepository.save(transformTodo(todoRequestTDO, todo));
+    }
+
+    @Override
+    public String deleteTodo(String id){
+        Long idTodo = Long.parseLong(id);
+        todoRepository.deleteById(idTodo);
+        return id;
+    }
+
+    private Todo transformTodo (TodoRequestTDO todoRequestTDO, Todo todo) {
+        todo.setUserTodo(userFind(todoRequestTDO.username()));
+        todo.setTitulo(todoRequestTDO.titulo());
+        todo.setDescricao(todoRequestTDO.descricao());
+        todo.setStatus(todoRequestTDO.status());
+        todo.setImportante(todoRequestTDO.importante());
+        return todo;
+    }
+    
+    private User userFind(String username){
+        User user = (User) userRepository.findByUsername(username);
+        if (user == null) {
+            throw new RuntimeException("Usuário não encontrado" + username);
+        }
+        return user;
+    }
 }
