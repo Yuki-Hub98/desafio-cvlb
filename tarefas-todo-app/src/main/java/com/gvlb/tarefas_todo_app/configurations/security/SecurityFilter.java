e3c06a18abf0331ad00b116234125384d6775602
Yuki-Hub98 <wellyson.yago1@gmail.com>
2025-04-12 10:31:12 -0400

    feat(erro autentication) - quando o usuario não estiver autentificado coloquei um try para retornar um erro tambem

--- tarefas-todo-app/src/main/java/com/gvlb/tarefas_todo_app/configurations/security/SecurityFilter.java
+++ tarefas-todo-app/src/main/java/com/gvlb/tarefas_todo_app/configurations/security/SecurityFilter.java
@@ -1,5 +1,7 @@
 package com.gvlb.tarefas_todo_app.configurations.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gvlb.tarefas_todo_app.DTO.ExceptionDTO;
 import com.gvlb.tarefas_todo_app.repositories.UserRepository;
 import com.gvlb.tarefas_todo_app.services.TokenService;
 import jakarta.servlet.FilterChain;
@@ -7,6 +9,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,15 +28,26 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var token = this.recoverToken(request);
-        if (token != null ){
-            var username = tokenService.validateToken(token);
-            UserDetails user = userRepository.findByUsername(username);
+        try {
+            var token = this.recoverToken(request);
 
-            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            if (token != null) {
+                var username = tokenService.validateToken(token);
+                UserDetails user = userRepository.findByUsername(username);
+
+                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+
+            filterChain.doFilter(request, response);
+
+        } catch (NullPointerException e) {
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+            response.setContentType("application/json");
+            response.getWriter().write(new ObjectMapper().writeValueAsString(
+                    new ExceptionDTO("Usuário não autenticado ou token inválido", "400")
+            ));
         }
-        filterChain.doFilter(request, response);
     }
 
     private String recoverToken(HttpServletRequest request){
