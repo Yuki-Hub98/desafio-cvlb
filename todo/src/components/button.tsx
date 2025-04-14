a63f788ea24b7f00f23d43b03a3f563a39ebfbff
Yuki-Hub98 <wellyson.yago1@gmail.com>
2025-04-14 00:36:29 -0400

    feat(button) mudando algumas caracteristicas do button para ficar mais maleavel

--- todo/src/components/button.tsx
+++ todo/src/components/button.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
 
 interface ButtonProps {
-  type: 'submit' | 'button' | 'reset'; // Adapte conforme necessário
-  onClick?: () => void; // Para o caso de botões com ações customizadas
+  type: 'submit' | 'button' | 'reset';
+  onClick?: () => void;
   className?: string;
   children: React.ReactNode;
 }
 
-export const Button: React.FC<ButtonProps> = ({
-  type,
-  onClick,
-  className,
-  children,
-}) => {
+export const Button: React.FC<ButtonProps> = ({ type, onClick, className, children }) => {
   return (
     <button
       type={type}
       onClick={onClick}
-      className={`w-full bg-green-500 text-green-600 py-3 rounded-xl hover:bg-green-600 transition duration-200 ${className}`}
+      className={`text-green-600 rounded-xl cursor-pointer hover:bg-green-600 hover:text-green-200  transition duration-200 ${className}`}
     >
       {children}
     </button>
