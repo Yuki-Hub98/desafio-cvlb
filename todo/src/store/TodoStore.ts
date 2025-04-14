97e2b43fbad75a5f308b247ac3c3fd25c2f7e696
Yuki-Hub98 <wellyson.yago1@gmail.com>
2025-04-14 00:34:15 -0400

    feat(state) - configurando metodos do state global, para adicionar e editar

--- todo/src/store/TodoStore.ts
+++ todo/src/store/TodoStore.ts
@@ -4,11 +4,18 @@ import { Todo } from '../models/todo';
 type TodoStore = {
   todos: Todo[];
   addToTodos: (item: Todo) => void;
+  addManyTodos: (items: Todo[]) => void;
+  updateTodo: (item: Todo) => void;
 };
 
 export const useTodoStore = create<TodoStore>((set) => {
   return {
     todos: [],
     addToTodos: (item) => set((state) => ({ todos: [...state.todos, item] })),
+    addManyTodos: (items: Todo[]) => set(() => ({ todos: items })),
+    updateTodo: (updatedTodo: Todo) =>
+      set((state) => ({
+        todos: state.todos.map((todo) => (todo.id === updatedTodo.id ? updatedTodo : todo)),
+      })),
   };
 });
